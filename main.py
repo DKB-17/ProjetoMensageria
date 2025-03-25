faf92fe987122f9a1a590e636537672a775a38d4
diego <136401880+DKB-17@users.noreply.github.com>
2025-03-25 18:54:47 -0300

    Envio de img pelo whats

--- main.py
+++ main.py
@@ -15,23 +15,36 @@ while len(navegador.find_elements(By.ID, "side")) < 1:
 
 contatos_df = openpyxl.load_workbook('contatos.xlsx')
 pagina_contatos = contatos_df['Planilha1']
+link_img = f'C:\\Users\\HomePC\\Desktop\\ProjetoMensageria\\beae38d0-e834-42c0-82ba-2bc00f7a87da.jpeg'
 
 for linha in pagina_contatos.iter_rows(min_row=2):
     nome = linha[0].value
     telefone = linha[1].value
-    mensagem = f"Ola {nome}!, Estou informando que deu certo o teste"
+    mensagem = f"Ola {nome}!, Estou testando o envio de mensagem automatizado, favor ignorar as mensagens"
 
     try:
-        link_mensagem_wt = f"https://web.whatsapp.com/send?phone={telefone}&text={quote(mensagem)}"
+        link_mensagem_wt = f"https://web.whatsapp.com/send?phone={telefone}"
         navegador.get(link_mensagem_wt)
 
         while len(navegador.find_elements(By.ID, "side")) < 1:
             time.sleep(1)
 
-        time.sleep(10)
-        navegador.find_element(By.XPATH,
-            '//*[@id="main"]/footer/div[1]/div/span/div/div[2]/div[1]/div/div/p/span').send_keys(Keys.ENTER)
-        time.sleep(10)
+        while len(navegador.find_elements(By.XPATH, '//*[@id="main"]/footer/div[1]/div/span/div/div[1]/div/button')) < 1:
+            time.sleep(1)
+
+        navegador.find_element(By.XPATH, '//*[@id="main"]/footer/div[1]/div/span/div/div[1]/div/button').click()
+
+        while len(navegador.find_elements(By.XPATH, '//*[@id="app"]/div/span[5]/div/ul/div/div/div[2]/li/div/input')) < 1:
+            time.sleep(1)
+
+        navegador.find_element(By.XPATH, '//*[@id="app"]/div/span[5]/div/ul/div/div/div[2]/li/div/input').send_keys(link_img)
+
+        while len(navegador.find_elements(By.XPATH, '//*[@id="app"]/div/div[3]/div/div[2]/div[2]/span/div/div/div/div[2]/div/div[2]/div[2]/div/div')) < 1:
+            time.sleep(1)
+
+        navegador.find_element(By.XPATH, '//*[@id="app"]/div/div[3]/div/div[2]/div[2]/span/div/div/div/div[2]/div/div[2]/div[2]/div/div').click()
+
+        time.sleep(10000)
 
     except:
         print(f"Nao foi possivel enviar mensagem para {nome}")
